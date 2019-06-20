63a818dcd46929c8799c84d7cd8d831dca20d572
AlexFenfgCiso <alfeng@cisco.com>
2019-06-20 23:54:46 +0800

    upload files:  json receiver  GPB receiver
     telemetry.proto
     uptime.proto
     qos.proto
     cisco.proto
    
    output json result
    
    update udp length limitation
    
    add telemetry prometheus lab directory
    
    add machine learning engine
    
    add insert telemetry data to influxdb
    
    add kafka producer and consumer
    
    add gRPC dial in telemetry collector
    
    add gRPC dial out telemetry collector

--- telemetry_grpc_dial_in_no_tls.py
+++ telemetry_grpc_dial_in_no_tls.py
@@ -3,21 +3,36 @@
 
     Cisco IOS XR Software, Version 6.4.1
 
+
+     _____________________________________
+    |   Encoding Technique   |    Code    |
+     -------------------------------------
+    |           gpb          |     2      |
+     -------------------------------------
+    |           gpbkv        |     3      |
+     -------------------------------------
+    |           json         |     4      |
+     -------------------------------------
+
 '''
+
 import grpc
 import telemetry_pb2
 import cisco_grpc_dialin_pb2
 import cisco_grpc_dialin_pb2_grpc
 from google.protobuf.json_format import MessageToJson
+import json
+import pprint
 
-
+encoding_code = 4
 channel = grpc.insecure_channel("10.75.58.60:57400")
 
 stub = cisco_grpc_dialin_pb2_grpc.gRPCConfigOperStub(channel)
 
 sub_id = 'test_sub' # Telemetry MDT subscription
 
-sub_args = cisco_grpc_dialin_pb2.CreateSubsArgs(ReqId=1, encode=3, subidstr=sub_id)
+sub_args = cisco_grpc_dialin_pb2.CreateSubsArgs(ReqId=1, encode=encoding_code, subidstr=sub_id)  #encoding code see doc
+
 
 timeout = float(100000)
 metadata = [('username', 'cisco'), ('password', 'cisco')]
@@ -25,8 +40,13 @@ stream = stub.CreateSubs(sub_args, timeout=timeout, metadata=metadata)
 
 for segment in stream:
     telemetry_pb = telemetry_pb2.Telemetry()
-    t = telemetry_pb.ParseFromString(segment.data)
-    # print json message
-    print(MessageToJson(telemetry_pb))
+    if encoding_code != 4:
+        t = telemetry_pb.ParseFromString(segment.data)
+        # print json message
+        print(MessageToJson(telemetry_pb))
+    else:
+        #print(json.loads(segment.data.decode(encoding='utf-8')))
+        pprint.pprint(json.loads(segment.data.decode(encoding='utf-8')))
+
 
 
